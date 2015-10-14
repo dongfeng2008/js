2323aa4d30585c88e64b3011869905e9598415c2
dongfeng2008 <1138897810@qq.com>
2015-10-14 14:12:52 +0800

    fsafas

--- yun.js
+++ yun.js
@@ -1,5 +1,26 @@
 
 
+var urls =[];
+$('.dir-handler').each(function(){
+		var url = $(this).attr('href');
+		urls.push(url);
+});
+urls.length =5;
+var timeout = false; //启动及关闭按钮
+function time()
+{
+  if(timeout) return;
+  if(urls.length !=0){
+  	var url = urls.pop();
+  	console.log('open : '+url);
+  	window.open(url);
+  }else{
+  	timeout = false;
+  }
+  setTimeout(time,10000); //time是指本身,延时递归调用自己,100为间隔调用时间,单位毫秒
+}
+
+time();
 
 
 console.log("保存文件");

bd2522fb6bb3e0544ccd45d2142e636a8bbbadae
dongfeng2008 <1138897810@qq.com>
2015-10-14 14:58:46 +0800

    fasf

--- yun.js
+++ yun.js
@@ -1,10 +1,8 @@
 
-var isCopy = true;
 var urls =[];
 $('.dir-handler').each(function(){
 		var url = $(this).attr('href');
 		urls.push(url);
-		isCopy = false;
 });
 /*if(urls.lenght!=0){
 	urls.length =5;
@@ -18,16 +16,16 @@ function time()
   	console.log('open : '+url);
   	window.open(url);
   }else{
-  	timeout = false;
-  	return ;
+  	timeout = true;
+  	return;
   }
   setTimeout(time,30000); //time是指本身,延时递归调用自己,100为间隔调用时间,单位毫秒
 }
 
 time();
 
-if(isCopy){
-	console.log("保存文件");
+
+console.log("保存文件");
 		console.log(window.yunData);
     var sharePath = "["+"\""+window.yunData.PATH+"\""+"]";
 	var shareId = window.yunData.SHARE_ID;
@@ -41,6 +39,3 @@ if(isCopy){
 			path:'/fafa'
 		}
 	);
-
-}
-
